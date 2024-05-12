f46001f431770aa6a4579019138fbd8d6feb7db1
farhanhameed <farhanabdulhameed@gmail.com>
2024-05-12 10:51:25 -0700

    push word count to github

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+// this line is called SHEBANG, it tells the OS to run it with node js
+
+// import the inquirer module, which is  command linde interface for Node js
+
+import inquirer from "inquirer";
+
+//  Declear a constant asnwers & assign it to the result of inquirer.
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt([{
+    name: "Sentence",
+    type: "input",
+    message: "Enter your sentence to count the word: "
+}])
+
+const words = answers.Sentence.trim().split(" ");
+
+// print the array of words to the console
+console.log(words);
+
+// print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
